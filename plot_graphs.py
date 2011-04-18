560697e4673144be63151d79f0de72cb24f3a4fb
Paul Wais <pwais@quad.local>
2011-04-18 02:36:05 -0700

    graph defs

--- plot_graphs.py
+++ plot_graphs.py
@@ -11,67 +11,119 @@ GRAPHS_COLS = 4
 
 GRAPHS = (
 	
-	# Uniform
+	# Uniform Distro
 
-	(# eps greedies
+	('Epsilon_Greedy_eps=0.001_Uniform_eps=0.01_K=100.json',
+	 'Epsilon_Greedy_eps=0.1_Uniform_eps=0.01_K=100.json',
+	 'Epsilon-t_Greedy_c=5.0_eps=0.1_Uniform_eps=0.01_K=100.json',
+	 'Epsilon-t_Greedy_c=1.0_eps=0.1_Uniform_eps=0.01_K=100.json',
+	 'Epsilon-t_Greedy_c=0.1_eps=0.1_Uniform_eps=0.01_K=100.json',
 	),
 	
 	('UCB_Uniform_eps=0.01_K=100.json',
+	 'UCB-Normal_Uniform_eps=0.01_K=100.json',
 	 'UCB-Bernoulli_Uniform_eps=0.01_K=100.json',
+	 'UCB2_alpha=0.01_Uniform_eps=0.01_K=100.json',
+	 'UCB2-Non-Sequential_alpha=0.01_Uniform_eps=0.01_K=100.json',
+	 'Poker_Uniform_eps=0.01_K=100.json',
 	),
 	
 	('EXP3_Uniform_eps=0.001_K=100.json',
+	 'EXP3_gamma=0.01_Uniform_eps=0.001_K=100.json',
+	 'SoftMix_Uniform_eps=0.001_K=100.json',
 	),
 	
-	(# explorers
+	('Naive_Explorer_eps=0.01_delta=0.01_Uniform_eps=0.01_K=100.json',
+	 'Successive_Elimination_delta=0.01_Uniform_eps=0.01_K=100.json',
+	 'Successive_Elimination_with_Unknown_Biases_eps=0.01_delta=0.01_Uniform_eps=0.01_K=100.json',
+	 'Median_Elimination_eps=0.01_delta=0.01_Uniform_eps=0.01_K=100.json',
 	),
 	
 	
 	# Normal
 
-	(# eps greedies
+	('Epsilon_Greedy_eps=0.001_Normal_K=100.json',
+	 'Epsilon_Greedy_eps=0.1_Normal_K=100.json',
+	 'Epsilon-t_Greedy_c=5.0_eps=0.1_Normal_K=100.json',
+	 'Epsilon-t_Greedy_c=1.0_eps=0.1_Normal_K=100.json',
+	 'Epsilon-t_Greedy_c=0.1_eps=0.1_Normal_K=100.json',
 	),
 	
 	('UCB_Normal_K=100.json',
+	 'UCB-Normal_Normal_K=100.json',
 	 'UCB-Bernoulli_Normal_K=100.json',
+	 'UCB2_alpha=0.01_Normal_K=100.json',
+	 'UCB2-Non-Sequential_alpha=0.01_Normal_K=100.json',
+	 'Poker_Normal_K=100.json',
 	),
 	
 	('EXP3_Normal_K=100.json',
+	 'EXP3_gamma=0.01_Normal_K=100.json',
+	 'SoftMix_Normal_K=100.json',
 	),
 	
-	(# explorers
+	('Naive_Explorer_eps=0.01_delta=0.01_Normal_K=100.json',
+	 'Successive_Elimination_delta=0.01_Normal_K=100.json',
+	 'Successive_Elimination_with_Unknown_Biases_eps=0.01_delta=0.01_Normal_K=100.json',
+	 'Median_Elimination_eps=0.01_delta=0.01_Normal_K=100.json',
 	),
 
 
 	# Bernoulli
 
-	(# eps greedies
+	('Epsilon_Greedy_eps=0.001_Bernoulli_K=100.json',
+	 'Epsilon_Greedy_eps=0.1_Bernoulli_K=100.json',
+	 'Epsilon-t_Greedy_c=5.0_eps=0.1_Bernoulli_K=100.json',
+	 'Epsilon-t_Greedy_c=1.0_eps=0.1_Bernoulli_K=100.json',
+	 'Epsilon-t_Greedy_c=0.1_eps=0.1_Bernoulli_K=100.json',
 	),
 	
 	('UCB_Bernoulli_K=100.json',
+	 'UCB-Normal_Bernoulli_K=100.json',
 	 'UCB-Bernoulli_Bernoulli_K=100.json',
+	 'UCB2_alpha=0.01_Bernoulli_K=100.json',
+	 'UCB2-Non-Sequential_alpha=0.01_Bernoulli_K=100.json',
+	 'Poker_Bernoulli_K=100.json',
 	),
 	
 	('EXP3_Bernoulli_K=100.json',
+	 'EXP3_gamma=0.01_Bernoulli_K=100.json',
+	 'SoftMix_Bernoulli_K=100.json',
 	),
 	
-	(# explorers
+	('Naive_Explorer_eps=0.01_delta=0.01_Bernoulli_K=100.json',
+	 'Successive_Elimination_delta=0.01_Bernoulli_K=100.json',
+	 'Successive_Elimination_with_Unknown_Biases_eps=0.01_delta=0.01_Bernoulli_K=100.json',
+	 'Median_Elimination_eps=0.01_delta=0.01_Bernoulli_K=100.json',
 	),
 	
 	
 	# Network Latencies
 
-	(# eps greedies
+	('Epsilon_Greedy_eps=0.001_Network_Latencies_K=100.json',
+	 'Epsilon_Greedy_eps=0.1_Network_Latencies_K=100.json',
+	 'Epsilon-t_Greedy_c=5.0_eps=0.1_Network_Latencies_K=100.json',
+	 'Epsilon-t_Greedy_c=1.0_eps=0.1_Network_Latencies_K=100.json',
+	 'Epsilon-t_Greedy_c=0.1_eps=0.1_Network_Latencies_K=100.json',
 	),
 	
-	('UCB_Network_Latencies_K=760.json',
-	 'UCB-Bernoulli_Network_Latencies_K=760.json',
+	('UCB_Network_Latencies_K=100.json',
+	 'UCB-Normal_Network_Latencies_K=100.json',
+	 'UCB-Bernoulli_Network_Latencies_K=100.json',
+	 'UCB2_alpha=0.01_Network_Latencies_K=100.json',
+	 'UCB2-Non-Sequential_alpha=0.01_Network_Latencies_K=100.json',
+	 'Poker_Network_Latencies_K=100.json',
 	),
 	
-	('EXP3_Network_Latencies_K=760.json',
+	('EXP3_Network_Latencies_K=100.json',
+	 'EXP3_gamma=0.01_Network_Latencies_K=100.json',
+	 'SoftMix_Network_Latencies_K=100.json',
 	),
 	
-	(# explorers
+	('Naive_Explorer_eps=0.01_delta=0.01_Network_Latencies_K=100.json',
+	 'Successive_Elimination_delta=0.01_Network_Latencies_K=100.json',
+	 'Successive_Elimination_with_Unknown_Biases_eps=0.01_delta=0.01_Network_Latencies_K=100.json',
+	 'Median_Elimination_eps=0.01_delta=0.01_Network_Latencies_K=100.json',
 	),
 )
 
@@ -80,7 +132,12 @@ def plot_graph(fnames):
 		return
 	
 	# Load all data
-	datas = [simplejson.load(open("records/%s" % fname)) for fname in fnames]
+	datas = []
+	for fname in fnames:
+		try:
+			datas.append(simplejson.load(open("records/%s" % fname)))
+		except Exception:
+			print "Probably bad file %s" % fname
 	
 	distro_name = datas[0][0]['distro_name']
 	
